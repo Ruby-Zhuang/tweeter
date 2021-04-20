d65df78ecf5349a1a7ab76d34edefd1a37dd4020
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-20 15:48:26 -0400

    Add new class exceeded-limit to change the colour of the counter if it exceeds the limit. Update logic in composer-char-counter.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -13,6 +13,15 @@ const inputEvent = function(event) {
   const charLeft = maxChar - charCounter;
   const counter = $(this).parent().find('.counter');
 
+  console.log(counter);
+  // Change counter colour if user exceeds character limit
+  if (charLeft < 0) {
+    counter.addClass('exceeded-limit');
+  } else {
+    counter.removeClass('exceeded-limit');
+  }
   counter.text(charLeft);
-
 };
+
+// 140 CHARACTERS TEST (DELETE AFTER)
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad miasd
