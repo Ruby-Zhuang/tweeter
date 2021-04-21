1bf6e48eb6cdf4a804ad84c88484f491318b9d99
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-21 12:52:01 -0400

    Implement createTweetElement function that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,14 +17,41 @@ const tweetData = {
 };
 
 const createTweetElement = function(tweet) {
-  let $tweet = /* Your code for creating the tweet element */
-  // ...
+  // Tweet header
+  const name = tweet.user.name;
+  const avatarSrc = tweet.user.avatars;
+  const handle = tweet.user.handle;
+  
+  // Tweet content
+  const tweetContent = tweet.content.text;
+  
+  // Tweet footer
+  const dateCreated = tweet.created_at;
+  const icons = `<i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>`;
+  
+  // Create tweet markup
+  const $tweet =
+    `<article class="tweet">
+      <header>
+        <span class="username"><img src="${avatarSrc}" alt="user-avatar"> ${name}</span>
+        <span class="handle">${handle}</span>
+      </header>
+      <p>${tweetContent}</p>
+      <footer>
+        <span class="timeago" datetime="${dateCreated}"></span>
+        <span>${icons}</span>
+      </footer>
+    </article>`;
+
   return $tweet;
-}
+};
 
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+$(document).ready(function() {
+  const $tweet = createTweetElement(tweetData);
+  
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
