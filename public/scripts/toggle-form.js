832a84c72415354920e3285fc35ef3f324f23706
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-21 22:13:26 -0400

    Style second toggle button in the lower right hand corner. Start implementing functionality to show button when user scrolls and to scroll back to the top if user clicks it.

--- public/scripts/toggle-form.js
+++ public/scripts/toggle-form.js
@@ -1,5 +1,5 @@
 /*
- * Client-side JS logic to implement form toggling using navbar compose section
+ * Client-side JS logic to implement form togglng
  * jQuery is already loaded
  */
 
@@ -8,6 +8,17 @@ const composeHandler = function() {
   $('#tweet-text').focus();
 };
 
+const handleScroll = function() {
+  $('.fa-angle-double-up').show();
+
+  $('.fa-angle-double-up').on('click', () => {
+    $('.new-tweet').slideDown();
+    $(window).scrollTop(0);
+    $('.fa-angle-double-up').hide();
+  });
+};
+
 $(document).ready(function() {
   $('.navbar').on('click', composeHandler);
+  $(window).on('scroll', handleScroll);
 });
